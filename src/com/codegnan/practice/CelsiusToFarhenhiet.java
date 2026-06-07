b1edd6c482251e4a5466177c31ddf7a913d3fa27
Aparanjini06 <aparanjinipallela1@gmail.com>
2026-06-07 13:38:20 +0530

    feat: add Celsius to Fahrenheit converter

---
+++ src/com/codegnan/practice/CelsiusToFarhenhiet.java
@@ -0,0 +1,18 @@
+package com.codegnan.git;
+
+import java.util.Scanner;
+
+public class CelsiusToFarhenhiet {
+
+	public static void main(String[] args) {
+		Scanner s= new Scanner(System.in);
+		System.out.println("Enter the celsius value:");
+		int celsius = s.nextInt();
+		
+		double farh = celsius* 9/5 +32;
+		System.out.println("celsius : "+celsius);
+		System.out.println("farhenheit : "+farh);
+		s.close();
+	}
+
+}
