c2f6168b8ec3e18e0b91d26aaf320c3d02b93f33
Aparanjini06 <aparanjinipallela1@gmail.com>
2026-06-07 13:37:32 +0530

    feat: add bank deposit calculation program

---
+++ src/com/codegnan/practice/BankDepositCal.java
@@ -0,0 +1,20 @@
+package com.codegnan.git;
+
+import java.util.Scanner;
+
+public class BankDepositCal {
+
+	public static void main(String[] args) {
+		Scanner s= new Scanner(System.in);
+		int amount = s.nextInt();
+		int deposit = s.nextInt();
+		
+		amount+=deposit;//assignment operator
+		
+		boolean isEvenAndGreater1000 = (amount>=1000)&&(amount%2==0);
+		System.out.println(amount);
+		System.out.println(isEvenAndGreater1000);
+		s.close();
+	}
+
+}
