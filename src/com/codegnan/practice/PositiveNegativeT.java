28b45750be8ffbd628e42ea0da11c38a6e8afcb8
Aparanjini06 <aparanjinipallela1@gmail.com>
2026-06-07 13:41:56 +0530

    feat: add positive and negative number checker

---
+++ src/com/codegnan/practice/PositiveNegativeT.java
@@ -0,0 +1,29 @@
+/*3. Positive or Negative Number
+Question:
+A banking system records transactions. Positive values represent deposits, while negative values represent withdrawals.
+Write a Java program that checks whether a given number is positive or negative using the ternary operator.
+Test Case 1
+Input:
+ 25
+Output:
+ Positive
+Test Case 2
+Input:
+ -15
+Output:
+ Negative*/
+package com.codegnan.git;
+
+import java.util.Scanner;
+
+public class PositiveNegativeT {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int num=s.nextInt();
+		String res= (num>=0)?"positive":"negative";
+		System.out.println(res);
+		s.close();
+	}
+
+}
