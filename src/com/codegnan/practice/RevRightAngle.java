601055f7e3057eab90579edc85f8e8029d3f4976
Aparanjini06 <aparanjinipallela1@gmail.com>
2026-06-07 13:42:48 +0530

    feat: add reverse right angle pattern

---
+++ src/com/codegnan/practice/RevRightAngle.java
@@ -0,0 +1,21 @@
+package com.codegnan.git;
+
+import java.util.Scanner;
+
+public class RevRightAngle {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int row=s.nextInt();
+		for(int i=1;i<=row;i++)
+		{
+			for(int j=row;j>=i;j--)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		s.close();
+	}
+
+}
