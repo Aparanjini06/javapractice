dd0856121c1b0b2f871db250c2b5f194ea6824b6
Aparanjini06 <aparanjinipallela1@gmail.com>
2026-06-07 13:38:36 +0530

    feat: add compound interest calculator

---
+++ src/com/codegnan/practice/CompoundIntrest.java
@@ -0,0 +1,23 @@
+package com.codegnan.git;
+
+import java.util.Scanner;
+
+public class CompoundIntrest {
+
+	public static void main(String[] args) {
+		Scanner s= new Scanner(System.in);
+		System.out.println("Enter principal amount:");
+		double principal = s.nextDouble();
+		System.out.println("Enter Rate of intrest: ");
+		double rate = s.nextDouble();
+		System.out.println("Enter time: ");
+		double time = s.nextDouble();
+		double amount= principal * Math.pow((1+rate/100.0),time);
+		double ci=amount-principal;
+		System.out.println("Amount:"+amount);
+		System.out.println("compound intrest : "+ci);
+		s.close();
+
+	}
+
+}
