c0e89ccef8e1739e0fd4baa8ecefc6974b5df4ba
Aparanjini06 <aparanjinipallela1@gmail.com>
2026-06-07 13:41:25 +0530

    feat: add odd and even position sum calculator

---
+++ src/com/codegnan/practice/OddEvenPositionSum.java
@@ -0,0 +1,37 @@
+package com.codegnan.git;
+//odd and even positions sum
+import java.util.Scanner;
+
+public class OddEvenPositionSum {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter the elements:");
+		int num=s.nextInt();
+		int []a=new int[num];
+		for(int i=0;i<a.length;i++)
+		{
+			a[i]=s.nextInt();
+		}
+		int evenSum=0;
+		int oddSum=0;
+		for(int i=0;i<a.length;i++)
+		{	
+			if(i%2==0)
+			{
+				oddSum+=a[i];
+			}
+			else {
+				evenSum+=a[i];
+			}
+			
+			
+		}
+		System.out.println("The sum of even positions:"+evenSum);
+		System.out.println("The sum of odd positions:"+oddSum);
+		s.close();
+	}
+
+}
+
+
