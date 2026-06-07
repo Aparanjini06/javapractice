73a144380724d065e37a116407d90fb4db86ea09
Aparanjini06 <aparanjinipallela1@gmail.com>
2026-06-07 13:40:08 +0530

    feat: add leap year checker

---
+++ src/com/codegnan/practice/LeapYearT.java
@@ -0,0 +1,29 @@
+/*2. Leap Year Checker
+Question:
+A calendar application needs to identify leap years to correctly calculate the number of days in February.
+Write a Java program that checks whether a given year is a leap year and displays the result using the ternary operator.
+Test Case 1
+Input:
+ 2024
+Output:
+ Leap Year
+Test Case 2
+Input:
+ 2023
+Output:
+ Not a Leap Year*/
+package com.codegnan.git;
+
+import java.util.Scanner;
+
+public class LeapYearT {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int year=s.nextInt();
+		String result =(year%4==0)?"Leap Year":"Non Leap Year";
+		System.out.println(result);
+		s.close();
+	}
+
+}
