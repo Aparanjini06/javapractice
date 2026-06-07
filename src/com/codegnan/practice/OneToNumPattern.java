d4780a52317dcf853ac77d5de99d980af2adbca0
Aparanjini06 <aparanjinipallela1@gmail.com>
2026-06-07 13:41:32 +0530

    feat: add one to N number pattern

---
+++ src/com/codegnan/practice/OneToNumPattern.java
@@ -0,0 +1,24 @@
+package com.codegnan.git;
+
+import java.util.Scanner;
+
+public class OneToNumPattern {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int row=s.nextInt();
+		int z=1;
+		for(int i=1;i<=row;i++)
+		{
+			for(int j=1;j<=i;j++)
+			{
+				
+				System.out.print(z++);
+				System.out.print(" ");
+			}
+			System.out.println();
+		}
+		s.close();
+	}
+
+}
