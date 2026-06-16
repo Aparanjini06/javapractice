3411bd740b78e958fed569e70b24c9df68b895c6
Aparanjini06 <aparanjinipallela1@gmail.com>
2026-06-16 14:05:39 +0530

    feat: add odd column sum for 2D array

---
+++ src/com/codegnan/practice/SumOddCol.java
@@ -0,0 +1,35 @@
+package com.codegnan.practice;
+
+import java.util.Scanner;
+
+public class SumOddCol {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int r=s.nextInt();
+		int c=s.nextInt();
+		int [][]arr=new int[r][c];
+		for(int i=0;i<r;i++)
+		{
+			for(int j=0;j<c;j++)
+			{
+				arr[i][j]=s.nextInt();
+				
+			}
+		}
+		int sum=0;
+		for(int i=0;i<r;i++)
+		{
+			for(int j=0;j<c;j++)
+			{
+				if(j%2!=0)
+				{
+					sum+=arr[i][j];
+				}
+			}
+		}
+		System.out.println(sum);
+		s.close();
+	}
+
+}
