5037f3aa1042e9912f5f94f2a52d6b6c401bfdee
Aparanjini06 <aparanjinipallela1@gmail.com>
2026-06-16 14:05:37 +0530

    feat: add row sum for 2D array

---
+++ src/com/codegnan/practice/RowSum2D.java
@@ -0,0 +1,32 @@
+package com.codegnan.practice;
+
+import java.util.Scanner;
+
+public class RowSum2D {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int r=s.nextInt();
+		int c=s.nextInt();
+		int [][]arr=new int[r][c];
+		for(int i=0;i<r;i++)
+		{
+			for(int j=0;j<c;j++)
+			{
+				arr[i][j]=s.nextInt();
+			}
+		}
+		
+		for(int i=0;i<r;i++)
+		{
+			int sum=0;
+			for(int j=0;j<c;j++)
+			{
+				sum+=arr[i][j];
+			}
+			System.out.println(sum);
+		}
+		s.close();
+	}
+
+}
