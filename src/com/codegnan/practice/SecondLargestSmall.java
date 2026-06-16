70a84b32589a05ea19bba0f8debd5f93ff6912f2
Aparanjini06 <aparanjinipallela1@gmail.com>
2026-06-16 14:05:41 +0530

    refactor: improve second largest and smallest array logic

--- src/com/codegnan/practice/SecondLargestSmall.java
+++ src/com/codegnan/practice/SecondLargestSmall.java
@@ -1,3 +1,37 @@
+/*You have been given an array a[ ] of n unique non-negative integers. Write a function to find the second largest and second smallest elements in the array. Return these two elements as another array of size 2.
+ 
+Input Format:
+An integer n, the size of the array.
+An array a[ ] of n unique non-negative integers.
+ 
+Output Format:
+An array of size 2 containing the second largest and second smallest elements.
+ 
+Sample Test Cases
+Test Case 1
+ Expected Output
+Enter the Size of the Array :
+4
+Enter the array Elements :
+3 4 5 1
+Second Largest and Second Smallest Elements : 4 3
+
+Test Case 2
+Expected Output
+Enter the Size of the Array :
+5
+Enter the array Elements :
+4 5 3 6 7
+Second Largest and Second Smallest Elements : 6 4
+ 
+Test Case 3
+Expected Output
+Enter the Size of the Array :
+5
+Enter the array Elements :
+10 20 30 40 50
+Second Largest and Second Smallest Elements : 40 20
+*/
 package com.codegnan.practice;
 import java.util.Scanner;
 
@@ -5,7 +39,9 @@ public class SecondLargestSmall {
 
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
+		System.out.println("Enter the size of the array :");
 		int row=s.nextInt();
+		System.out.println("Enter the array Elements :");
 		int []a=new int[row];
 		for(int i=0;i<row;i++) {
 			a[i]=s.nextInt();
@@ -30,8 +66,8 @@ public class SecondLargestSmall {
 			//System.out.println("second smallest element:"+a[i+1]);
 			//System.out.println("second largest element:"+a[row-1]);
 		}
-		System.out.println(secSmall);
-		System.out.println(secLargest);
+		System.out.println("Second Largest and Second Smallest Elements: "+secLargest+" "+secSmall);
+		
 		s.close();
 	}
 
