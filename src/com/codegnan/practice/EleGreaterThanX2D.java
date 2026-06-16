434cf951b5f1c1c65c6a7b39841988c5ee3cbbe9
Aparanjini06 <aparanjinipallela1@gmail.com>
2026-06-16 14:05:32 +0530

    feat: add elements greater than X in 2D array

---
+++ src/com/codegnan/practice/EleGreaterThanX2D.java
@@ -0,0 +1,37 @@
+package com.codegnan.practice;
+
+import java.util.Scanner;
+
+public class EleGreaterThanX2D {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int r=s.nextInt();
+		int c=s.nextInt();
+		int [][]arr=new int[r][c];
+		for(int i=0;i<r;i++)
+		{
+			for(int j=0;j<c;j++)
+			{
+				arr[i][j]=s.nextInt();
+				
+			}
+		}
+		int x=s.nextInt();
+		int count=0;
+		
+		for(int i=0;i<r;i++)
+		{
+			for(int j=0;j<c;j++)
+			{
+				if(arr[i][j]>x)
+				{
+					count++;
+				}
+			}
+		}
+		System.out.println(count);
+		s.close();
+	}
+
+}
