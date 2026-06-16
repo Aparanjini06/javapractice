a7d09aef48800a579c2b64046fc3ec508988c660
Aparanjini06 <aparanjinipallela1@gmail.com>
2026-06-16 14:05:29 +0530

    feat: add positive element count in jagged array

---
+++ src/com/codegnan/practice/CountPosJagged.java
@@ -0,0 +1,35 @@
+package com.codegnan.practice;
+
+import java.util.Scanner;
+
+public class CountPosJagged {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int r=s.nextInt();
+		int arr[][]=new int[r][];
+		for(int i=0;i<r;i++)
+		{
+			int c=s.nextInt();
+			arr[i]=new int[c];
+			for(int j=0;j<c;j++)
+			{
+				arr[i][j]=s.nextInt();
+			}
+		}
+		int count=0;
+		for(int i=0;i<r;i++)
+		{
+			for(int j=0;j<arr[i].length;j++)
+			{
+				if(arr[i][j]>0)
+				{
+					count++;
+				}
+			}
+		}
+		System.out.println(count);
+		s.close();
+	}
+
+}
