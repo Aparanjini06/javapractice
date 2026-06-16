496b9d677c21b6483029e722dd689d92f6b8e6ab
Aparanjini06 <aparanjinipallela1@gmail.com>
2026-06-16 14:05:34 +0530

    feat: add main diagonal sum for 2D array

---
+++ src/com/codegnan/practice/MainDiagonalSum2D.java
@@ -0,0 +1,37 @@
+package com.codegnan.practice;
+
+import java.util.Scanner;
+
+public class MainDiagonalSum2D {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int r=s.nextInt();
+		int c=s.nextInt();
+		int [][]arr=new int[r][c];
+		for(int i=0;i<r;i++)
+		{
+			for(int j=0;j<c;j++)
+			{
+				arr[i][j]=s.nextInt();
+				
+			}
+		}
+		
+		int total=0;
+		for(int i=0;i<r;i++)
+		{
+			for(int j=0;j<c;j++)
+			{
+				if(arr[i]==arr[j])
+				{
+					total+=arr[i][j];
+				}
+			}
+		}
+		System.out.println(total);
+		s.close();
+	}
+	
+
+}
